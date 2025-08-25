319a5ce32da8566ee3f0032da45aae6ab5d116c7
Danylo Melnychuk <danyakid2005@gmail.com>
2025-08-25 12:32:55 +0300

    Fix bootstrap.sh instructions and env.valueFrom.configMapKeyRef.name

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
+# Create kind cluster
+kind create cluster --config cluster.yml
+
 kubectl apply -f .infrastructure/security/ns.yml
 
 helm dependency update ./helm-chart/todoapp
 helm upgrade --install todoapp ./helm-chart/todoapp -n todoapp --create-namespace
 
 kubectl get pods -n todoapp
+
+kubectl get all,cm,secret,ing -A > output.log
